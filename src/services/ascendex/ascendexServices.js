80830a385026814d3291df6b3e498ea35c051142
Ashwin Shukla <ashwin72shukla@gmail.com>
2024-08-23 00:14:54 +0530

    minor

--- src/services/ascendex/ascendexServices.js
+++ src/services/ascendex/ascendexServices.js
@@ -1,13 +1,19 @@
-import axios from 'axios';
 import config from '../../config.js';
+import { fetchDataHelper } from '../helper/index.js';
 
 export const ascendex = () => {
 	const {
 		platformApisBasePath: { ascendexAPI },
 	} = config;
 	return {
-		klineData: (req, res) => fetchData(`${ascendexAPI}klines`, req, res),
-		currentPrice: (req, res) => fetchData(`${ascendexAPI}ticker/price`, req, res),
-		orderBook: (req, res) => fetchData(`${ascendexAPI}depth`, req, res),
+		klineData: async (req, res) => {
+			let data = await fetchDataHelper(`${ascendexAPI}klines`, req, res);
+		},
+		currentPrice: async (req, res) => {
+			let data = await fetchDataHelper(`${ascendexAPI}ticker/price`, req, res);
+		},
+		orderBook: async (req, res) => {
+			let data = await fetchDataHelper(`${ascendexAPI}depth`, req, res);
+		},
 	};
 };
--- src/services/binance/binanceServices.js
+++ src/services/binance/binanceServices.js
@@ -1,13 +1,28 @@
-import axios from 'axios';
 import config from '../../config.js';
+import { fetchDataHelper } from '../helper/index.js';
 
 export const binance = () => {
 	const {
 		platformApisBasePath: { binanceAPI },
 	} = config;
 	return {
-		klineData: (req, res) => fetchData(`${binanceAPI}klines`, req, res),
-		currentPrice: (req, res) => fetchData(`${binanceAPI}ticker/price`, req, res),
-		orderBook: (req, res) => fetchData(`${binanceAPI}depth`, req, res),
+		klineData: async (req, res) => {
+			let data = await fetchDataHelper(`${binanceAPI}klines`, req, res);
+      if (!data || data?.length === 0) {
+        return res.status(404).json({ error: true, message: "Data not found"})
+      }
+		},
+		currentPrice: async (req, res) => {
+			let data = await fetchDataHelper(`${binanceAPI}ticker/price`, req, res);
+      if (!data || data?.length === 0) {
+        return res.status(404).json({ error: true, message: "Data not found"})
+      }
+		},
+		orderBook: async (req, res) => {
+			let data = await fetchDataHelper(`${binanceAPI}depth`, req, res);
+      if (!data || data?.length === 0) {
+        return res.status(404).json({ error: true, message: "Data not found"})
+      }
+		},
 	};
 };
