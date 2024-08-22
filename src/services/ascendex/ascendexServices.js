ad8211ab6914de3f347356c43649719f9c1e6e13
Ashwin Shukla <ashwin72shukla@gmail.com>
2024-08-23 01:13:29 +0530

    Implement autogen swagger docs

--- src/services/ascendex/ascendexServices.js
+++ src/services/ascendex/ascendexServices.js
@@ -7,13 +7,25 @@ export const ascendex = () => {
 	} = config;
 	return {
 		klineData: async (req, res) => {
-			let data = await fetchDataHelper(`${ascendexAPI}klines`, req, res);
+			const data = await fetchDataHelper(`${ascendexAPI}klines`, req, res);
+      if (!data || data?.length === 0) {
+				return res.status(404).json({ error: true, message: 'Data not found' });
+			}
+			return res.status(200).json({success: true, data: data})
 		},
 		currentPrice: async (req, res) => {
-			let data = await fetchDataHelper(`${ascendexAPI}ticker/price`, req, res);
+			const data = await fetchDataHelper(`${ascendexAPI}ticker/price`, req, res);
+      if (!data || data?.length === 0) {
+				return res.status(404).json({ error: true, message: 'Data not found' });
+			}
+			return res.status(200).json({success: true, data: data})
 		},
 		orderBook: async (req, res) => {
-			let data = await fetchDataHelper(`${ascendexAPI}depth`, req, res);
+			const data = await fetchDataHelper(`${ascendexAPI}depth`, req, res);
+      if (!data || data?.length === 0) {
+				return res.status(404).json({ error: true, message: 'Data not found' });
+			}
+			return res.status(200).json({success: true, data: data})
 		},
 	};
 };
--- src/services/express/index.js
+++ src/services/express/index.js
@@ -5,7 +5,9 @@ import cors from 'cors';
 import express from 'express';
 import helmet from 'helmet';
 import morgan from 'morgan';
-import config from '../../config.js';
+import swaggerUI from 'swagger-ui-express';
+// import config from '../../config.js';
+import swaggerDoc from '../swagger/swagger-output.json' assert { type: 'json' };
 import 'dotenv/config';
 
 export default routes => {
@@ -13,18 +15,20 @@ export default routes => {
 
 	app.use(express.json());
 	app.use(compression());
-	app.use(bodyParser.json());
-	app.use(bodyParser.urlencoded({ extended: false }));
-	app.use(cookieParser());
+	
 	app.use(cookieParser());
 	app.use(express.urlencoded({ extended: true }));
-	app.use(helmet());
-	app.use(cors(config.corsConfig));
+	
+  app.use(helmet());
+  app.use(cors())
+	// app.use(cors(config.corsConfig));
 	app.use(morgan('dev'));
-
+  app.get('/health-check', (req, res) => {
+    return res.status(200).json({ success: true, message: 'Server working fine'})
+  })
+	app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 	app.use((err, req, res, next) => {
 		res.status(500).json({ error: true, message: err.message });
 	});
-
 	return app;
 };
