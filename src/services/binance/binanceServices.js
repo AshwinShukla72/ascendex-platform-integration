d60b01534459efd90f0fcf35b5acea0d4dfb21c3
Ashwin Shukla <ashwin72shukla@gmail.com>
2024-08-22 22:08:37 +0530

    refactor: :art: Simple refactoring for fetch APIs

--- src/services/binance/binanceServices.js
+++ src/services/binance/binanceServices.js
@@ -6,57 +6,8 @@ export const binance = () => {
 		platformApisBasePath: { binanceAPI },
 	} = config;
 	return {
-		klineData: async (req, res) => {
-			try {
-				const apiConfig = {
-					url: `${binanceAPI}klines`,
-					method: 'GET',
-					data: { query: req.query },
-				};
-				const data = await axios(apiConfig);
-				if (!data || data?.length === 0) {
-					return res.status(404).json({ error: true, message: 'Data not found' });
-				}
-				return res.status(200).json({ success: true, data });
-			} catch (error) {
-				console.log(`Error in ${functionName} -->`, console.log(error));
-				return res.status(500).json({ error: true, message: error.message || 'Somthing Went Wrong' });
-			}
-		},
-		currentPrice: async (req, res) => {
-			try {
-				const apiConfig = {
-					url: `${binanceAPI}ticker/price`,
-					method: 'GET',
-					data: { query: req.query },
-				};
-				const data = await axios(apiConfig);
-				if (!data || data?.length === 0) {
-					return res.status(404).json({ error: true, message: 'Data not found' });
-				}
-        
-				return res.status(200).json({ success: true, data });
-			} catch (error) {
-				console.log(`Error in ${functionName} -->`, console.log(error));
-				return res.status(500).json({ error: true, message: error.message || 'Somthing Went Wrong' });
-			}
-		},
-		orderBook: async (req, res) => {
-			try {
-				const apiConfig = {
-					url: `${binanceAPI}depth`,
-					method: 'GET',
-					data: { query: req.query },
-				};
-				const data = await axios(apiConfig);
-				if (!data || data?.length === 0) {
-					return res.status(404).json({ error: true, message: 'Data not found' });
-				}
-				return res.status(200).json({ success: true, data });
-			} catch (error) {
-				console.log(`Error in ${functionName} -->`, console.log(error));
-				return res.status(500).json({ error: true, message: error.message || 'Somthing Went Wrong' });
-			}
-		},
+		klineData: (req, res) => fetchData(`${binanceAPI}klines`, req, res),
+		currentPrice: (req, res) => fetchData(`${binanceAPI}ticker/price`, req, res),
+		orderBook: (req, res) => fetchData(`${binanceAPI}depth`, req, res),
 	};
 };
--- src/services/helper/index.js
+++ src/services/helper/index.js
@@ -1,3 +1,22 @@
-export const helper = () => {
-	console.log("I'm just a helper function");
+// Axios fetchDataHelper
+export const fetchDataHelper = async (url, req, res) => {
+	const functionName = 'fetchDataHelper';
+	try {
+		const apiConfig = {
+			url: url,
+			method: 'GET',
+			params: req.query,
+		};
+		console.log('Request Config -->', apiConfig);
+		const data = await axios(apiConfig);
+		if (!data || data.length === 0) {
+			return res.status(404).json({ error: true, message: 'Data not found' });
+		}
+		return res.status(200).json({ success: true, data });
+	} catch (error) {
+		console.error(`Error in ${functionName} -->`, error);
+		return res.status(500).json({ error: true, message: error.message || 'Something went wrong' });
+	}
 };
+
+// export const standardResponseFormatter = (success, data = null, message = '') => ({ success, data, message });
