098d67302873f4ebe60f114f0709f6681f37497b
Ashwin Shukla <ashwin72shukla@gmail.com>
2024-08-23 10:27:04 +0530

    Refactor ascendex order-book & current price API

--- src/services/ascendex/ascendexServices.js
+++ src/services/ascendex/ascendexServices.js
@@ -12,22 +12,24 @@ export const ascendex = () => {
 			if (!response || response?.length === 0) {
 				return res.status(404).json({ error: true, message: 'Data not found' });
 			}
-			const requiredData = response?.data?.data
-			return res.status(200).json({ success: true, data: requiredData});
+			const requiredData = response?.data?.data;
+			return res.status(200).json({ success: true, data: requiredData });
 		},
 		currentPrice: async (req, res) => {
-			const data = await fetchDataHelper(`${ascendexAPI}ticker/price`, req, res);
-			if (!data || data?.length === 0) {
+			const { symbol } = req.query
+			const response= await fetchDataHelper(`${ascendexAPI}trades`, { symbol }, res);
+			if (!response || response?.length === 0) {
 				return res.status(404).json({ error: true, message: 'Data not found' });
 			}
-			return res.status(200).json({ success: true, data: data });
+			return res.status(200).json({ success: true, data: response?.data?.data });
 		},
 		orderBook: async (req, res) => {
-			const data = await fetchDataHelper(`${ascendexAPI}depth`, req, res);
-			if (!data || data?.length === 0) {
+			const { symbol } = req.query
+			const response= await fetchDataHelper(`${ascendexAPI}trades`, { symbol }, res);
+			if (!response || response?.length === 0) {
 				return res.status(404).json({ error: true, message: 'Data not found' });
 			}
-			return res.status(200).json({ success: true, data: data });
+			return res.status(200).json({ success: true, data: response?.data?.data });
 		},
 	};
 };
